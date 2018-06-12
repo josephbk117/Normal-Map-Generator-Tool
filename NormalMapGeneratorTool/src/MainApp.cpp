3daeeecb5b9028ce26e8495a54e5efdb8722d08c
joseph kalathil <josephbk.117@hotmail.com>
2018-06-13 00:46:32 +0530

    new ui

--- NormalMapGeneratorTool/src/MainApp.cpp
+++ NormalMapGeneratorTool/src/MainApp.cpp
@@ -11,8 +11,8 @@
 #include "ShaderProgram.h"
 #include "Transform.h"
 
-int windowWidth;
-int windowHeight;
+int windowWidth = 1200;
+int windowHeight = 1200;
 GLFWwindow* window;
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 bool isKeyPressed(int key);
@@ -22,9 +22,9 @@ int main(void)
 {
 	if (!glfwInit())
 		return -1;
-	window = glfwCreateWindow(1200, 1200, "Normal Map Editor v0.1 alpha", NULL, NULL);
+	window = glfwCreateWindow(windowWidth, windowHeight, "Normal Map Editor v0.1 alpha", NULL, NULL);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	if (!window)
 	{
@@ -52,9 +52,6 @@ int main(void)
 	//ImGui::StyleColorsClassic();
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-	int windowWidth = 1200;
-	int windowHeight = 1200;
-
 	DrawingPanel drawingPanel;
 	drawingPanel.init(1.0f, 1.0f);
 	unsigned int texId = TextureManager::loadTextureFromFile("Resources\\goli.png", "poke", false);
@@ -67,11 +64,11 @@ int main(void)
 	int modelMatrixUniform = shader.getUniformLocation("model");
 	float strValue = 0.0f;
 	float zoomLevel = 0.0005f;
-	int normalMapMode = 1;
+	int normalMapMode = 3;
 	glm::vec3 rotation = glm::vec3(0);
 	Transform transform;
 	transform.setPosition(0, 0);
-
+	int k = 0;
 	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 	bool showHeightMapInput = true;
 	while (!glfwWindowShouldClose(window))
@@ -113,24 +110,41 @@ int main(void)
 		}
 
 		//---- Making sure the dimensions do not change for drawing panel ----//
-		float aspectRatio = windowWidth / windowHeight;
+		float aspectRatio = (float)windowWidth / (float)windowHeight;
 		transform.setScale(glm::vec2(windowWidth / aspectRatio, windowHeight * aspectRatio) * zoomLevel);
 		transform.setX(glm::clamp(transform.getPosition().x, -0.3f, 0.3f));
 		transform.setY(glm::clamp(transform.getPosition().y, -0.3f, 0.3f));
 		transform.update();
 
 		//---- Applying Shader Uniforms---//
+		
 		shader.applyShaderUniformMatrix(modelMatrixUniform, transform.getMatrix());
 		shader.applyShaderFloat(strengthValueUniform, strValue);
 		shader.applyShaderInt(normalMapModeOnUniform, normalMapMode);
 		shader.use();
 		drawingPanel.draw();
-
+		
 		ImGui_ImplGlfwGL3_NewFrame();
 
-		ImGui::SliderFloat("Normal Strength", &strValue, -50.0f, 50.0f);
-		if (ImGui::Button("Toggle View Mode"))
-			normalMapMode = (normalMapMode + 1) % 3 + 1;
+		ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0);
+		ImGui::PushStyleVar(ImGuiStyleVar_Alpha, 0.95f);
+		ImGuiWindowFlags window_flags = 0;
+		window_flags |= ImGuiWindowFlags_NoMove;
+		window_flags |= ImGuiWindowFlags_NoResize;
+		window_flags |= ImGuiWindowFlags_NoCollapse;
+		bool *p_open = NULL;
+		
+		ImGui::SetNextWindowPos(ImVec2(0, 0), ImGuiSetCond_Always);
+		ImGui::SetNextWindowSize(ImVec2(glm::clamp(windowWidth * 0.15f, 250.0f, 600.0f), windowHeight), ImGuiSetCond_Always);
+		ImGui::Begin("Settings", p_open, window_flags);
+		ImGui::Combo("Inputs Mode", &k, "All Inputs\0No Inputs\0RGB Input\0HSV Input\0HEX Input\0");
+		ImGui::PushItemWidth(ImGui::GetContentRegionAvailWidth() * 0.45f);
+		if (ImGui::DragFloat("Normal Strength", &strValue, 0.1f, -100.0f, 100.0f, "X: %.3f")) {}
+		ImGui::PopItemWidth();
+		ImGui::End();
+		ImGui::PopStyleVar();
+		ImGui::PopStyleVar();
+
 
 		ImGui::Render();
 		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
