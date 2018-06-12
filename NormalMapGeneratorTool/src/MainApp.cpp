bea2b708a6b2154cc641f512a7988a0f77673a23
joseph kalathil <josephbk.117@hotmail.com>
2018-06-12 21:56:09 +0530

    added ui controls

--- NormalMapGeneratorTool/src/MainApp.cpp
+++ NormalMapGeneratorTool/src/MainApp.cpp
@@ -3,6 +3,9 @@
 #include <GL\glew.h>
 #include <GLFW/glfw3.h>
 
+#include "imgui.h"
+#include "imgui_impl_glfw_gl3.h"
+
 #include "DrawingPanel.h"
 #include "TextureLoader.h"
 #include "ShaderProgram.h"
@@ -20,25 +23,40 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 	window = glfwCreateWindow(1200, 1200, "Normal Map Editor v0.1 alpha", NULL, NULL);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1);
 	glewExperimental = GL_TRUE;
 	if (glewInit() != GLEW_OK)
 	{
 		std::cout << "Open GL init error" << std::endl;
 		return EXIT_FAILURE;
 	}
+	// Setup Dear ImGui binding
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO(); (void)io;
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;   // Enable Gamepad Controls
+	ImGui_ImplGlfwGL3_Init(window, true);
+
+	// Setup style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 	int windowWidth = 1200;
 	int windowHeight = 1200;
 
 	DrawingPanel drawingPanel;
-	drawingPanel.init(0.8f, 0.8f);
+	drawingPanel.init(1.0f, 1.0f);
 	unsigned int texId = TextureManager::loadTextureFromFile("Resources\\goli.png", "poke", false);
 	drawingPanel.setTextureID(texId);
 	ShaderProgram shader;
@@ -47,16 +65,18 @@ int main(void)
 	int strengthValueUniform = shader.getUniformLocation("_HeightmapStrength");
 	int normalMapModeOnUniform = shader.getUniformLocation("_normalMapModeOn");
 	int modelMatrixUniform = shader.getUniformLocation("model");
-	float strValue = 4.0f;
-	float contrastStrength = 1.0f;
+	float strValue = 0.0f;
 	float zoomLevel = 0.0005f;
 	int normalMapMode = 1;
 	glm::vec3 rotation = glm::vec3(0);
 	Transform transform;
 	transform.setPosition(0, 0);
 
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+	bool showHeightMapInput = true;
 	while (!glfwWindowShouldClose(window))
 	{
+		glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		if (isKeyPressed(GLFW_KEY_1))
@@ -75,12 +95,12 @@ int main(void)
 		if (isKeyPressed(GLFW_KEY_DOWN))
 			transform.setY(transform.getPosition().y - 0.005f);
 		if (isKeyPressed(GLFW_KEY_8))
-			transform.setRotation(transform.getRotation() + 0.001f);
+			transform.setRotation(transform.getRotation() + 0.06f);
 
 		if (isKeyPressed(GLFW_KEY_A))
-			strValue += 0.01f;
+			strValue += 0.05f;
 		if (isKeyPressed(GLFW_KEY_D))
-			strValue -= 0.01f;
+			strValue -= 0.05f;
 		if (isKeyPressed(GLFW_KEY_W))
 			zoomLevel += 0.000003f;
 		if (isKeyPressed(GLFW_KEY_S))
@@ -106,10 +126,21 @@ int main(void)
 		shader.use();
 		drawingPanel.draw();
 
+		ImGui_ImplGlfwGL3_NewFrame();
+
+		ImGui::SliderFloat("Normal Strength", &strValue, -50.0f, 50.0f);
+		if (ImGui::Button("Toggle View Mode"))
+			normalMapMode = (normalMapMode + 1) % 3 + 1;
+
+		ImGui::Render();
+		ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-
+	ImGui_ImplGlfwGL3_Shutdown();
+	ImGui::DestroyContext();
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
