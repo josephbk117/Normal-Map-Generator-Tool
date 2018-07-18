97c18810834e7979ee8056ef2ad29f0f03c1f685
joseph kalathil <josephbk.117@hotmail.com>
2018-07-19 00:55:56 +0530

    code clean up

--- NormalMapGeneratorTool/src/WindowTransformUtility.h
+++ NormalMapGeneratorTool/src/WindowTransformUtility.h
@@ -1,6 +1,6 @@
 #pragma once
 enum class WindowSide { NONE = -1, LEFT = 0, TOP_LEFT = 1, TOP = 2, TOP_RIGHT = 3, RIGHT = 4, BOTTOM_RIGHT = 5, BOTTOM = 6, BOTTOM_LEFT = 7 };
-static class WindowTransformUtility
+class WindowTransformUtility
 {
 public:
 	static const int BORDER_SIZE = 5;
