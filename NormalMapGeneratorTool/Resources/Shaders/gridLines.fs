73dfbc5b0a35d453a7e7910e4abebd02c3e8456f
joseph kalathil <josephbk.117@hotmail.com>
2019-01-15 23:45:36 +0530

    better grid shader

--- NormalMapGeneratorTool/Resources/Shaders/gridLines.fs
+++ NormalMapGeneratorTool/Resources/Shaders/gridLines.fs
@@ -9,10 +9,9 @@ uniform float _CameraZoom;
 
 void main()
 {
-	float valX = TexCoords.x * 1000.0;
-	float valY = TexCoords.y * 1000.0;
-	float val = 0.0;
-	val = step(mod(valX, 10),9) * step(mod(valY, 10), 9);
-	val = 1.0 - val;
+	float valX = TexCoords.x * 1500.0;
+	float valY = TexCoords.y * 1500.0;
+	float val = pow(sin(smoothstep(8.5, 10.0, mod(valX, 10)) * 3.1415926), 8);
+	val += pow(sin(smoothstep(8.5, 10.0, mod(valY, 10)) * 3.1415926), 8);
 	FragColor = vec4(val,val,val, (1.0 - (Depth * 0.02)) * val);
 }
